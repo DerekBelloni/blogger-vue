fb5a0c7321a63ac604fe12225c83c3a942b8d758
Derek <derekbelloni10@gmail.com>
2022-03-10 17:08:44 -0700

    updated modal

---
+++ src/pages/ActiveBlogPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6 rounded m-5 text-center shadow bg-light">
+        <h1>Active Blog</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
